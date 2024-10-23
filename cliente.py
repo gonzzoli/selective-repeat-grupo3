f3c7b7a5d10f26c7eebaf321a53654153b234088
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2024-10-23 19:41:58 -0300

    casi que anda

--- cliente.py
+++ cliente.py
@@ -1,37 +1,41 @@
 import socket
 
-puertoServidor = 12000
-socketServidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+puerto_cliente = 12000
+socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-socketServidor.bind(("", puertoServidor))
+socket_cliente.bind(("", puerto_cliente))
 
-expected_frame = 0
-received_buffer = {}
+marco_esperado = 0
+buffer = {}
+
+def enviar_ack(numero_marco, direccion_servidor):
+    print("ENVIANDO ACK", numero_marco)
+    socket_cliente.sendto(f"{numero_marco}".encode(), direccion_servidor)
 
 while True:
-    mensaje, direccionCliente = socketServidor.recvfrom(2048)
-    frame_num, data = mensaje.decode().split(":")
-    frame_num = int(frame_num)
+    mensaje, direccion_servidor = socket_cliente.recvfrom(2048)
+    nro_marco_recibido, mensaje = mensaje.decode().split(":")
+    nro_marco_recibido = int(nro_marco_recibido)
 
-    print(f"Recibido marco {frame_num}: {data}")
+    print(f"Recibido marco {nro_marco_recibido}: {mensaje}")
 
     # Si es el marco que esperamos, lo procesamos
-    if frame_num == expected_frame:
-        print(f"Procesando marco {frame_num}")
-        expected_frame += 1
+    if nro_marco_recibido == marco_esperado:
+        print(f"Procesando marco {nro_marco_recibido}")
+        # Ya recibimos y enviamos ACK, pasamos a esperar el proximo.
+        marco_esperado += 1
 
         # Revisar buffer por marcos fuera de orden
-        while expected_frame in received_buffer:
-            print(f"Procesando marco en buffer {expected_frame}: {received_buffer[expected_frame]}")
-            del received_buffer[expected_frame]
-            expected_frame += 1
+        while marco_esperado in buffer:
+            print(f"Procesando marco en buffer {marco_esperado}: {buffer[marco_esperado]}")
+            del buffer[marco_esperado]
+            # Si los hay, significa que podemos avanzarlos sin esperar por otra respuesta
+            marco_esperado += 1
 
     # Si es un marco fuera de orden, lo guardamos en el buffer
-    elif frame_num > expected_frame:
-        print(f"Marco fuera de orden {frame_num}, almacenado en buffer")
-        received_buffer[frame_num] = data
-
-    # Enviar ACK para el marco actual
-    print("ENVIANDO ACK", expected_frame)
-    socketServidor.sendto(f"{expected_frame}".encode(), direccionCliente)
+    elif nro_marco_recibido > marco_esperado:
+        print(f"Marco fuera de orden {nro_marco_recibido}, almacenado en buffer")
+        buffer[nro_marco_recibido] = mensaje
+    
+    enviar_ack(nro_marco_recibido, direccion_servidor)
 
--- servidor.py
+++ servidor.py
@@ -8,26 +8,26 @@ import random
 nombreServidor = 'localhost'
 puertoServidor = 12000
 TAMANO_VENTANA = 4
-TIMEOUT = 2
+TIMEOUT = 3
 
-socketCliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 mensajes = ["0:mensaje", "1:mensaje", "2:mensaje", "3:mensaje"]
 acknowledged = set()
 
-proximaSecuencia = 0
 temporizadores = {}
 
 def iniciar_temporizador(numero_marco):
     temporizadores[numero_marco] = time.time()
 
-def verificar_temporizador(numero_marco):
-    if time.time() - temporizadores[numero_marco] > TIMEOUT:
+def temporizador_vencido(numero_marco):
+    if numero_marco in temporizadores and time.time() - temporizadores[numero_marco] > TIMEOUT:
         return True
     return False
 
 def simular_marco_perdido():
     aleatorio = random.random()
-    if aleatorio > 0.5:
+    if aleatorio > 0.2:
         return False
     return True
 
@@ -38,15 +38,16 @@ def enviar_marco(numero_marco):
     if simular_marco_perdido():
         print(f"Marco {numero_marco} perdido")
         return
-    socketCliente.sendto(mensaje.encode(), (nombreServidor, puertoServidor))
+    socket_servidor.sendto(mensaje.encode(), (nombreServidor, puertoServidor))
 
 def recibir_ack():
-    while len(mensajes) != len(acknowledged):
+    while len(mensajes) > len(acknowledged):
         try:
-            ack, _ = socketCliente.recvfrom(2048)
+            ack, _ = socket_servidor.recvfrom(2048)
             ack_num = int(ack.decode())
             print(f"Recibido ACK para marco {ack_num}")
-            acknowledged.add(ack_num)
+            acknowledged.add(ack_num) # Marcamos el paquete como recibido
+            temporizadores.pop(ack_num) # Eliminamos el temporizador del paquete recibido
         except socket.timeout:
             break
         except Exception as e:
@@ -54,30 +55,39 @@ def recibir_ack():
             break
 
 def cliente():
-    
-    global proximaSecuencia
-    socketCliente.settimeout(TIMEOUT)
+    secuencia_base = 0
+    socket_servidor.settimeout(TIMEOUT)
 
     # Hilo para manejar los ACKs
-    ack_handler = threading.Thread(target=recibir_ack)
-    ack_handler.start()
+    receptor_ack = threading.Thread(target=recibir_ack)
+    receptor_ack.start()
 
-    while proximaSecuencia < len(mensajes):
-        for numero_marco in range(proximaSecuencia, min(proximaSecuencia + TAMANO_VENTANA, len(mensajes))):
-            if numero_marco not in acknowledged:
-                time.sleep(1)
-                enviar_marco(numero_marco)
-
-        for numero_marco in range(proximaSecuencia, min(proximaSecuencia + TAMANO_VENTANA, len(mensajes))):
-            if numero_marco not in acknowledged and verificar_temporizador(numero_marco):
-                print(f"Retransmitiendo marco {numero_marco}")
-                time.sleep(1)
-                enviar_marco(numero_marco)
-
-        proximaSecuencia = min(proximaSecuencia + TAMANO_VENTANA, len(mensajes))
+    while secuencia_base < len(mensajes):
         time.sleep(1)
+        print(f"Secuencia base: {secuencia_base}")
+        print(f"Nro mensajes: {len(mensajes)} - Recibidos: {len(acknowledged)}")
+        print(acknowledged)
 
-    socketCliente.close()
+        for numero_marco in range(secuencia_base, min(secuencia_base + TAMANO_VENTANA, len(mensajes))):
+            if numero_marco not in acknowledged and numero_marco not in temporizadores.keys():
+                # Condiciones que cumplen los paquetes que aun no se envian ni por 1era vez
+                print(f"1era vez: {numero_marco}")
+                enviar_marco(numero_marco)
+                time.sleep(1)
+            
+            for numero_marco in temporizadores.keys():
+                # Paquetes que ya se enviaron (tienen temporizador)
+                if temporizador_vencido(numero_marco) and numero_marco not in acknowledged:
+                    print(f"Marco {numero_marco} vencido")
+                    enviar_marco(numero_marco)
+
+        while secuencia_base in acknowledged:
+            # Si se han recibido los ACK de paquetes en la secuencia base, se desplaza la ventana
+            # hasta el primer paquete no ACK desde la base
+            secuencia_base += 1
+        print(acknowledged)
+
+    socket_servidor.close()
 
 if __name__ == "__main__":
     cliente()
