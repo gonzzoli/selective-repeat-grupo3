2cee43568c1d7e11abe02ff2ea66bb82890ece1b
Juan Ignacio Diaz <ignaciodiaz93@gmail.com>
2024-10-25 11:29:44 -0300

    mensaje generada

--- emisor.py
+++ emisor.py
@@ -112,5 +112,5 @@ if __name__ == "__main__":
 
     for i in range(cantidad_mensajes):
         mensajes.append(f"{i}:mensaje {i}")
-    print(f"Mensajes generadas: {mensajes}")
+    print(f"Mensajes generados: {mensajes}")
     emisor()
