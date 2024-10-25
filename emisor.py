a4f2d6efa0c0462d9c5eae5bb7cd30c9c7d7258f
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2024-10-25 10:08:02 -0300

    checksum y valores parametrizables

---
+++ emisor.py
@@ -0,0 +1,116 @@
+import socket
+import threading
+import time
+import random
+import hashlib
+
+# Hace el selective repeat
+
+host_receptor = 'localhost'
+puerto_receptor = 12001
+
+socket_emisor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+acknowledged = set()
+temporizadores = {}
+
+# Valores asignados en el __main__, estos son por defecto
+mensajes = []
+TAMANO_VENTANA = 4
+TIMEOUT = 2
+PROBABILIDAD_PERDIDA = 0.2
+PROBABILIDAD_CORRUPCION = 0.2
+
+
+def iniciar_temporizador(numero_trama):
+    temporizadores[numero_trama] = time.time()
+
+def temporizador_vencido(numero_trama):
+    if numero_trama in temporizadores and time.time() - temporizadores[numero_trama] > TIMEOUT:
+        return True
+    return False
+
+def simular_trama_perdida():
+    aleatorio = random.random()
+    if aleatorio > PROBABILIDAD_PERDIDA:
+        return False
+    return True
+
+def simular_trama_corrupta():
+    aleatorio = random.random()
+    if aleatorio > PROBABILIDAD_CORRUPCION:
+        return False
+    return True
+
+def enviar_trama(numero_trama):
+    mensaje_con_secuencia = mensajes[numero_trama]
+    # Generamos un hash como checksum, sobre el payload del mensaje, y solo tomamos 6 caracteres para no hacerla tan larga
+    mensaje_con_checksum = f"{hashlib.md5(mensaje_con_secuencia.split(":")[1].encode()).hexdigest()[:6]}:{mensaje_con_secuencia}"
+    # Solo se envian mensajes que no han sido enviados o cuyo temporizador venció, por eso los string posibles del print
+    print(f"Enviando trama {"por primera vez " if numero_trama not in temporizadores else 'vencida '} {numero_trama}: {mensaje_con_checksum}")
+    iniciar_temporizador(numero_trama)
+    if simular_trama_perdida():
+        print(f"Trama {numero_trama} perdida")
+        return
+    
+    if simular_trama_corrupta():
+        print(f"Trama {numero_trama} corrompida")
+        # Si se corrompió, modificamos algo en el payload para simular ese error, y el checksum sera distinto en el receptor
+        socket_emisor.sendto((f"{mensaje_con_checksum}" + "a").encode(), (host_receptor, puerto_receptor))
+    else:
+        socket_emisor.sendto(mensaje_con_checksum.encode(), (host_receptor, puerto_receptor))
+
+def recibir_ack():
+    while len(mensajes) > len(acknowledged):
+        print("Esperando ACK")
+        try:
+            ack, _ = socket_emisor.recvfrom(2048)
+            ack_num = int(ack.decode())
+            print(f"Recibido ACK para trama {ack_num}")
+            acknowledged.add(ack_num) # Marcamos el paquete como recibido
+            temporizadores.pop(ack_num) # Eliminamos el temporizador del paquete recibido
+        except socket.timeout:
+            print("TIMEOUT")
+        except Exception as e:
+            print(f"Error al recibir ACK: {e}")
+
+def emisor():
+    secuencia_base = 0
+    socket_emisor.settimeout(TIMEOUT)
+
+    # Hilo para manejar los ACKs
+    receptor_ack = threading.Thread(target=recibir_ack)
+    receptor_ack.start()
+
+    # Se ejecuta mientras queden tramas por enviar
+    while secuencia_base < len(mensajes):
+        print(f"Secuencia base: {secuencia_base}")
+        print(f"Nro mensajes: {len(mensajes)} - Recibidos: {len(acknowledged)}")
+        print(acknowledged)
+        time.sleep(1)
+
+        # Por cada trama dentro de la ventana actualmente analiza su estado y las envia o no dependiendo de ello
+        for numero_trama in range(secuencia_base, min(secuencia_base + TAMANO_VENTANA, len(mensajes))):
+            if numero_trama not in acknowledged and( numero_trama not in temporizadores.keys() or temporizador_vencido(numero_trama)):
+                # Condiciones que generan un envío de la trama
+                enviar_trama(numero_trama)
+                time.sleep(1)
+
+        while secuencia_base in acknowledged:
+            # Si se han recibido los ACK de paquetes en la secuencia base, se desplaza la ventana
+            # hasta el primer paquete no ACK desde la base
+            secuencia_base += 1
+            print(f"Secuencia base incrementada: {secuencia_base}")
+
+    socket_emisor.close()
+
+if __name__ == "__main__":
+    cantidad_mensajes = int(input("Ingrese la cantidad de mensajes a enviar: "))
+    PROBABILIDAD_PERDIDA = float(input("Ingrese la probabilidad de perdida de trama: "))
+    PROBABILIDAD_CORRUPCION = float(input("Ingrese la probabilidad de corrupcion de trama: "))
+    TIMEOUT = int(input("Ingrese el timeout de las tramas: "))
+    TAMANO_VENTANA = int(input("Ingrese el tamaño de la ventana: "))
+
+    for i in range(cantidad_mensajes):
+        mensajes.append(f"{i}:mensaje {i}")
+    print(f"Mensajes generadas: {mensajes}")
+    emisor()
---
+++ receptor.py
@@ -0,0 +1,48 @@
+import socket
+import hashlib
+
+puerto_receptor = 12001
+socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+socket_cliente.bind(("", puerto_receptor))
+
+trama_esperada = 0
+buffer = {}
+
+def enviar_ack(numero_trama, direccion_servidor):
+    print("ENVIANDO ACK", numero_trama)
+    socket_cliente.sendto(f"{numero_trama}".encode(), direccion_servidor)
+
+while True:
+    mensaje, direccion_servidor = socket_cliente.recvfrom(2048)
+    print(f"RECIBIDO DESDE {direccion_servidor}")
+    checksum, nro_trama_recibida, mensaje = mensaje.decode().split(":")
+    nro_trama_recibida = int(nro_trama_recibida)
+
+    print(f"Recibida trama {nro_trama_recibida}: {checksum}:{nro_trama_recibida}:{mensaje}")
+
+    # Si el checksum no coincide, ignoramos la trama y esperamos la proxima
+    if hashlib.md5(mensaje.encode()).hexdigest()[:6] != checksum:
+        print("Checksum incorrecto")
+        continue
+
+    # Si es la trama que esperamos, la procesamos
+    if nro_trama_recibida == trama_esperada:
+        print(f"Procesando trama {nro_trama_recibida}")
+        # Ya recibimos y enviamos ACK, pasamos a esperar el proximo.
+        trama_esperada += 1
+
+        # Revisar buffer por tramas fuera de orden
+        while trama_esperada in buffer:
+            print(f"Procesando trama en buffer {trama_esperada}: {buffer[trama_esperada]}")
+            del buffer[trama_esperada]
+            # Si los hay, significa que podemos avanzarlos sin esperar por otra respuesta
+            trama_esperada += 1
+
+    # Si es una trama fuera de orden, la guardamos en el buffer
+    elif nro_trama_recibida > trama_esperada:
+        print(f"Trama fuera de orden {nro_trama_recibida}, almacenada en buffer")
+        buffer[nro_trama_recibida] = mensaje
+    
+    enviar_ack(nro_trama_recibida, direccion_servidor)
+
